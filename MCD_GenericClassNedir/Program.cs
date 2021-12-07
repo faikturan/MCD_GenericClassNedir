1f1af702e243a6892dda9d21f2ae5131a80d6f0f
Faik Turan <turanfaik@hotmail.com>
2021-12-07 20:12:55 +0300

    code changed

--- MCD_GenericClassNedir/Program.cs
+++ MCD_GenericClassNedir/Program.cs
@@ -10,6 +10,27 @@ namespace MCD_GenericClassNedir
     {
         static void Main(string[] args)
         {
+            Musteri M1 = new Musteri();
+            M1.id = 1;
+            M1.TcKimlikNumarasi = "12254401254";
+            M1.musteriNumara = "MS123";
+            M1.isim = "Faik";
+            M1.soyisim = "Turan";
+            M1.DogumTarih = DateTime.Parse("20.07.1978");
+
+            MusteriGeneric<int> MusteriGeneric1 = new MusteriGeneric<int>();
+            MusteriGeneric1.id = 1;
+            MusteriGeneric1.musteriNumarasiAl();
+
+            MusteriGeneric<Guid> MusteriGeneric2 = new MusteriGeneric<Guid>();
+            Console.WriteLine(MusteriGeneric2.musteriNumarasiAl()); 
+
+
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
