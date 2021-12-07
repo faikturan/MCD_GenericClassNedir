b5ee6906d0356c6a4d5eecbec8b1a25952659ebc
Faik Turan <turanfaik@hotmail.com>
2021-12-07 20:42:16 +0300

    code changed

--- MCD_GenericClassNedir/Program.cs
+++ MCD_GenericClassNedir/Program.cs
@@ -10,8 +10,31 @@ namespace MCD_GenericClassNedir
     {
         static void Main(string[] args)
         {
-          
 
+            /*
+             * Database   | Uygulama
+             * MSSQL         C#
+             * 
+             * 
+             * tblMusteri
+             * id int
+             * musteriNumara uniqueidentityfy (Guid)
+             * isim nvarchar(50) string
+             * soyisim nvarchar(50) string
+             * dogumTarih datetime datetime
+             * 
+             * UnitOfWorks => Generic Class | Generic Interface
+             */
+
+            genericRepository<Musteri> repositoryMusteri = new genericRepository<Musteri>();
+
+            List<Musteri> musterilerim = repositoryMusteri.Getir();
+
+            repositoryMusteri.yeniKayitEkle(null);
+
+            genericRepository<Urun> repositoryUrun = new genericRepository<Urun>();
+            repositoryUrun.Getir();
+            repositoryUrun.yeniKayitEkle(null);
 
         }
     }
